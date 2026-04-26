f34ff71d306d1f98235efd7f37330cf95a2c775d
Ebenezhar Selvakumar <103163165+Ebenezhar@users.noreply.github.com>
2026-04-26 19:22:24 +0530

    test: add failing test for employee model [RED]

--- employees/tests.py
+++ employees/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
+from employees.models import Employee
 
-# Create your tests here.
+class EmployeeModelTest(TestCase):
+    def test_create_employee(self):
+        employee = Employee.objects.create(
+            full_name="Ebenezhar Balu",
+            job_title="Senior Developer",
+            country="India",
+            salary=100000
+        )
+        self.assertEqual(employee.full_name, "Ebenezhar Balu")

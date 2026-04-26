541f73c26f0f6aa7e30b5b542e76b16e1e10b428
Ebenezhar Selvakumar <103163165+Ebenezhar@users.noreply.github.com>
2026-04-26 23:28:38 +0530

    refactor: upated the EmployeeRecordListCreateAPIView with the get method to get list of the employees

--- employees/views.py
+++ employees/views.py
@@ -1,9 +1,19 @@
-from rest_framework.generics import ListCreateAPIView
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
 from employees.models import EmployeeRecord
 from employees.serializers import EmployeeSerializer
 
 
-class EmployeeRecordListCreateAPIView(ListCreateAPIView):
-    queryset = EmployeeRecord.objects.all()
-    serializer_class = EmployeeSerializer
+class EmployeeRecordListCreateAPIView(APIView):
+    def get(self, request):
+        employees = EmployeeRecord.objects.all()
+        serializer = EmployeeSerializer(employees, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def post(self, request):
+        serializer = EmployeeSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)

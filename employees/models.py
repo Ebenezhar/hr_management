edd1ba96aee949552174247b9631e15b047c7aac
Ebenezhar Selvakumar <103163165+Ebenezhar@users.noreply.github.com>
2026-04-26 19:55:25 +0530

    Chore: Table name is updated from Employee to EmployeeRecord to improve the code readability

--- employees/models.py
+++ employees/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-class Employee(models.Model):
+class EmployeeRecord(models.Model):
     full_name = models.CharField(max_length=255)
     job_title = models.CharField(max_length=255)
     country = models.CharField(max_length=100)
